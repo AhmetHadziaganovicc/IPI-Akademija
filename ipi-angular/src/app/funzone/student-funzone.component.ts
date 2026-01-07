ea085bc4c5c9ea52df0ee4f1c6766bf11e932032
Ahmet <ahmethadziaganovic22@gmail.com>
2026-01-07 22:13:36 +0100

    REMOVED: ipi-angular folder - moved to separate repository WP_2_Ahmet_Hadziaganovic

--- ipi-angular/src/app/funzone/student-funzone.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-student-funzone',
-  standalone: true,
-  templateUrl: './student-funzone.component.html',
-  styleUrls: ['./student-funzone.component.css']
-})
-export class StudentFunzoneComponent {}
